f7b053c03adef4f4742812f55f31a1aed096604a
tomocat <tomocat@gitee.com>
2022-10-15 12:47:24 +0800

    html example

--- 1.BeginToLearn/1.1 声明式渲染.vue
+++ 1.BeginToLearn/1.1 声明式渲染.vue
@@ -1,6 +1,7 @@
 <script>
-// 
+// 导出默认模块, 外部可以直接使用 message 和 counter
 export default {
+    // 创建一个属性属性, 它包含 message 和 counter 两个成员
     data() {
         return {
             message: 'Hello World',
